474ff7d69362c26cbab3373b62fb9023a2c6d5a1
Hadrian Tang <hadrianwttang@outlook.com>
2019-09-03 17:18:29 +0800

    Font test

--- SBA/Program.vb
+++ SBA/Program.vb
@@ -1,7 +1,58 @@
 ﻿Imports System.Console
-Imports System.Drawing
+Imports System.ConsoleColor
 
 Module SunnysBigAdventure
+    Structure Point
+        Public Sub New(x As Integer, y As Integer)
+            Me.X = x
+            Me.Y = y
+        End Sub
+        Public ReadOnly Property X As Integer
+        Public ReadOnly Property Y As Integer
+        Public Overrides Function ToString() As String
+            Return $"({X}, {Y})"
+        End Function
+    End Structure
+    Structure Rectangle
+        Public Sub New(top As Integer, left As Integer, width As Integer, height As Integer)
+            Me.New(New Point(top, left), width, height)
+        End Sub
+        Public Sub New(topLeft As Point, bottomRight As Point)
+            Me.New(topLeft, bottomRight.X - topLeft.X + 1, bottomRight.Y - topLeft.Y + 1)
+        End Sub
+        Public Sub New(topLeft As Point, width As Integer, height As Integer)
+            Me.TopLeft = topLeft
+            Me.Width = width
+            Me.Height = height
+        End Sub
+        Public ReadOnly Property TopLeft As Point
+        Public ReadOnly Property Width As Integer
+        Public ReadOnly Property Height As Integer
+        Public ReadOnly Property Left As Integer
+            Get
+                Return TopLeft.X
+            End Get
+        End Property
+        Public ReadOnly Property Right As Integer
+            Get
+                Return TopLeft.X + Width - 1
+            End Get
+        End Property
+        Public ReadOnly Property Top As Integer
+            Get
+                Return TopLeft.Y
+            End Get
+        End Property
+        Public ReadOnly Property Bottom As Integer
+            Get
+                Return TopLeft.Y + Height - 1
+            End Get
+        End Property
+        Public Overrides Function ToString() As String
+            Return $"({Left}, {Top}) to ({Right}, {Bottom})"
+        End Function
+    End Structure
+
     Property CursorPosition As Point
         Get
             Return New Point(CursorLeft, CursorTop)
@@ -11,42 +62,48 @@ Module SunnysBigAdventure
         End Set
     End Property
 
-    Dim Rectangles As New List(Of Rectangle)
-    Dim Sprites As New List(Of (Point As Point, C As Char))
+    ReadOnly Rectangles As New List(Of Rectangle)
+    ReadOnly Sprites As New List(Of (Point As Point, Display As String, Color As ConsoleColor))
 
     Sub Redraw()
+        ResetColor()
         For Each rect In Rectangles
-            CursorPosition = rect.Location
-            Dim innerRectWidth = rect.Width - 2
-            Dim innerRectHeight = rect.Height - 2
+            CursorPosition = rect.TopLeft
             Write("┏"c)
-            For i = 1 To innerRectWidth
+            For i = 1 To rect.Width - 2
                 Write("━"c)
             Next
             Write("┓"c)
-            For y = rect.Y + 1 To rect.Y + innerRectHeight - 1
-                SetCursorPosition(rect.X, y)
+            For y = 1 To rect.Height - 2
+                SetCursorPosition(rect.Left, y)
                 Write("┃"c)
-                SetCursorPosition(rect.X + innerRectWidth + 1, y)
+                SetCursorPosition(rect.Right, y)
                 Write("┃"c)
             Next
-            SetCursorPosition(rect.X, rect.Y + innerRectHeight)
+            SetCursorPosition(rect.Left, rect.Bottom)
             Write("┗"c)
-            For i = 1 To innerRectWidth
+            For i = 1 To rect.Width - 2
                 Write("━"c)
             Next
             Write("┛"c)
         Next
         For Each sprite In Sprites
             CursorPosition = sprite.Point
-            Write(sprite.C)
+            ForegroundColor = sprite.Color
+            Write(sprite.Display)
         Next
     End Sub
 
     Sub Main()
         OutputEncoding = Text.Encoding.UTF8
-        Rectangles.Add(New Rectangle(0, 0, 3, 3))
-        Sprites.Add((New Point(1, 1), "x"c))
+        WindowWidth = 64
+        WindowHeight = 48
+        Rectangles.Add(New Rectangle(New Point(0, 0), 2, 2))
+        Sprites.Add((New Point(3, 3), "☺", White))
+        Sprites.Add((New Point(3, 4), "☹", White))
+        Sprites.Add((New Point(3, 5), "😃", White))
+        Sprites.Add((New Point(3, 6), "☼", White))
         Redraw()
+        ReadKey(True)
     End Sub
 End Module
